958e37732528f94394f43ea6f0fd0a823ef1d084
pprudnikova10-stack <pprudnikova10@gmail.com>
2025-10-02 00:36:29 +0300

    Add files via upload

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -1,4 +1,5 @@
-﻿namespace Lab2
+﻿
+namespace Lab2
 {
     public class White
     {
@@ -6,85 +7,101 @@
         public int Task1(int n)
         {
             int answer = 0;
-
-            // code here
-
-            // end
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 3 * i - 1;
+            }
 
             return answer;
         }
         public double Task2(int n)
         {
             double answer = 0;
-
-            // code here
-
-            // end
+            for (double i = 1; i <= n; i++)
+            {
+                answer += 1 / i;
+            }
 
             return answer;
         }
         public long Task3(int n)
         {
-            long answer = 0;
+            long answer = 1;
 
-            // code here
-
-            // end
+            for (int i = 1; i <= n; i++)
+            {
+                answer *= i;
+            }
 
             return answer;
         }
         public long Task4(int a, int b)
         {
-            long answer = 0;
+            long answer = 1;
+            for (int i = 0; i < b; i++)
+            {
+                answer *= a;
+            }
 
-            // code here
 
-            // end
 
             return answer;
         }
         public int Task5(int L)
         {
-            int answer = 0;
+            int n = 1;
+            int p = 1;
+            while (p <= L)
+            {
+                n += 3;
+                p *= n;
+            }
 
-            // code here
-
-            // end
-
-            return answer;
+            return n;
         }
         public double Task6(double x)
         {
             double answer = 0;
 
-            // code here
+            for (double i = 1; i > 0.0001; i *= x * x)
+            {
+                if ((x > -1) && (x < 1))
+                {
+                    answer += i;
 
-            // end
+                }
 
+            }
             return answer;
         }
 
         public int Task7(int n)
         {
+
             int answer = 0;
-
-            // code here
-
-            // end
+            int sum = 0;
+            while (sum < n)
+            {
+                answer++;
+                sum += answer;
+            }
 
             return answer;
         }
-        public int Task8(double L, double v)
+        public int Task8(double L, double V)
         {
-            int answer = 0;
-            const double R = 6371.0; // радиус Земли, км
+            int t = 0;
+            const double R = 6371.0;
+            while (Math.Sqrt(V * t * (V * t + 2 * R)) <= L)
+            {
+                t++;
+            }
 
-            // code here
-
-            // end
-
-            return answer;
+            return t;
         }
     }
+
+
+   
 
 }
